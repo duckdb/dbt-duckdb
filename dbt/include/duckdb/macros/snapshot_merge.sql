e9ef50916c8be1eb8de2b25b92f553065bf9b856
Josh Wills <josh.wills@gmail.com>
2020-10-22 22:27:20 -0700

    Re-order the operations in the snapshot_merge impl

--- dbt/include/duckdb/macros/snapshot_merge.sql
+++ dbt/include/duckdb/macros/snapshot_merge.sql
@@ -1,21 +1,21 @@
 {% macro duckdb__snapshot_merge_sql(target, source, insert_cols) -%}
-    {% set update_sql %}
-    update {{ target }}
-    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
-    from {{ source }} as DBT_INTERNAL_SOURCE
-    where DBT_INTERNAL_SOURCE.dbt_scd_id = {{ target.identifier }}.dbt_scd_id
-      and DBT_INTERNAL_SOURCE.dbt_change_type = 'update'
-      and {{ target.identifier }}.dbt_valid_to is null;
-    {% endset %}
-
-    {% do adapter.add_query(update_sql) %}
-
     {%- set insert_cols_csv = insert_cols | join(', ') -%}
 
+    {% set insert_sql %}
     insert into {{ target }} ({{ insert_cols_csv }})
     select {% for column in insert_cols -%}
         DBT_INTERNAL_SOURCE.{{ column }} {%- if not loop.last %}, {%- endif %}
     {%- endfor %}
     from {{ source }} as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_change_type = 'insert';
+    {% endset %}
+
+    {% do adapter.add_query(insert_sql, auto_begin=False) %}
+
+    update {{ target }}
+    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+    from {{ source }} as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_scd_id = {{ target.identifier }}.dbt_scd_id
+      and DBT_INTERNAL_SOURCE.dbt_change_type = 'update'
+      and {{ target.identifier }}.dbt_valid_to is null;
 {% endmacro %}
