851dba86f6d41b0326a36061913ff46578d2b037
Josh Wills <josh.wills@gmail.com>
2022-04-04 21:09:39 -0700

    Use the new dbt adapter test library

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import os
+
+# Import the standard functional fixtures as a plugin
+# Note: fixtures with session scope need to be local
+pytest_plugins = ["dbt.tests.fixtures.project"]
+
+# The profile dictionary, used to write out profiles.yml
+# dbt will supply a unique schema per test, so we do not specify 'schema' here
+@pytest.fixture(scope="class")
+def dbt_profile_target():
+    return {
+        'type': 'duckdb',
+        'threads': 1,
+        'path': '/tmp/dbt_test.duckdb',
+    }
---
+++ tests/functional/adapter/test_basic.py
@@ -0,0 +1,46 @@
+import pytest
+
+from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
+from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
+from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
+from dbt.tests.adapter.basic.test_empty import BaseEmpty
+from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
+from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
+from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
+
+class TestSimpleMaterializationsMyAdapter(BaseSimpleMaterializations):
+    pass
+
+
+class TestSingularTestsMyAdapter(BaseSingularTests):
+    pass
+
+
+class TestSingularTestsEphemeralMyAdapter(BaseSingularTestsEphemeral):
+    pass
+
+
+class TestEmptyMyAdapter(BaseEmpty):
+    pass
+
+
+class TestEphemeralMyAdapter(BaseEphemeral):
+    pass
+
+
+class TestIncrementalMyAdapter(BaseIncremental):
+    pass
+
+
+class TestGenericTestsMyAdapter(BaseGenericTests):
+    pass
+
+
+class TestSnapshotCheckColsMyAdapter(BaseSnapshotCheckCols):
+    pass
+
+
+class TestSnapshotTimestampMyAdapter(BaseSnapshotTimestamp):
+    pass
