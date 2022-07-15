a9f2a28255cb9dd16af704480e82ae661033d429
Josh Wills <josh.wills@gmail.com>
2022-07-15 07:03:10 -0700

    WIP for 1.2.0rc1; may be basis of dbt-duckdb 1.2.0

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -11,7 +11,14 @@ from dbt.tests.adapter.basic.test_incremental import BaseIncremental
 from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
 from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
-
+from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+from dbt.tests.adapter.basic.test_validate_connection import BaseValidateConnection
+from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate, BaseDocsGenReferences
+from dbt.tests.adapter.basic.expected_catalog import (
+    base_expected_catalog,
+    no_stats,
+    expected_references_catalog,
+)
 
 class TestSimpleMaterializationsDuckDB(BaseSimpleMaterializations):
     pass
@@ -47,3 +54,41 @@ class TestSnapshotCheckColsDuckDB(BaseSnapshotCheckCols):
 
 class TestSnapshotTimestampDuckDB(BaseSnapshotTimestamp):
     pass
+
+class TestBaseAdapterMethodDuckDB(BaseAdapterMethod):
+    pass
+
+
+class TestValidateConnectionDuckDB(BaseValidateConnection):
+    pass
+
+
+class TestDocsGenerateDuckDB(BaseDocsGenerate):
+    @pytest.fixture(scope="class")
+    def expected_catalog(self, project):
+        return base_expected_catalog(
+            project,
+            role=None,
+            id_type="INTEGER",
+            text_type="VARCHAR",
+            time_type="TIMESTAMP",
+            view_type="VIEW",
+            table_type="BASE TABLE",
+            model_stats=no_stats()
+        )
+
+class TestDocsGenReferencesDuckDB(BaseDocsGenReferences):
+    @pytest.fixture(scope="class")
+    def expected_catalog(self, project):
+        return expected_references_catalog(
+            project,
+            role=None,
+            id_type="INTEGER",
+            text_type="VARCHAR",
+            time_type="TIMESTAMP",
+            view_type="VIEW",
+            table_type="BASE TABLE",
+            model_stats=no_stats(),
+            bigint_type="BIGINT",
+        )
+
