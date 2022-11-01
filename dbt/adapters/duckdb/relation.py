482ae3007d9e886a8ab2dc95fc071c09dde11748
James McNeill <james.mcneill@conjura.com>
2022-11-01 12:12:32 +0000

    fix deps, use proper identifier for files as this is what the materialization writes

--- dbt/adapters/duckdb/relation.py
+++ dbt/adapters/duckdb/relation.py
@@ -1,10 +1,17 @@
 import os
-from typing import Optional, TypeVar, Type
+from typing import Optional, TypeVar, Type, Any
 
 from dataclasses import dataclass
 
+import dbt
+
 from dbt.adapters.base.relation import BaseRelation
 from dbt.dataclass_schema import StrEnum
+
+from dbt.exceptions import InternalException
+from dbt.node_types import NodeType
+from dbt.contracts.graph.compiled import CompiledNode
+from dbt.contracts.graph.parsed import ParsedSourceDefinition, ParsedNode
 
 Self = TypeVar("Self", bound="DuckDBRelation")
 
@@ -21,13 +28,13 @@ class DuckDBRelation(BaseRelation):
 
     def render(self) -> str:
         if self.type in ["csv", "parquet"]:
-            db_dir = os.path.dirname(self.database)
-            db_dir += ("/" if db_dir != "" else "")
+            # db_dir = os.path.dirname(self.database)
+            # db_dir += ("/" if db_dir != "" else "")
             if self.type == "csv":
                 # TODO: use read_csv_auto(& infer params from config)... see https://duckdb.org/docs/data/csv
-                return f"{db_dir+self.database}.csv"
+                return f"'{self.identifier}.csv'"
             # read_parquet isn't truly required, but I'm making this explicit so that config can be passed if desired.
             if self.type == "parquet":
-                return f"read_parquet({db_dir+self.database}.parquet)"
+                return f"read_parquet('{self.identifier}.parquet')"
         else:
             return super().render()
