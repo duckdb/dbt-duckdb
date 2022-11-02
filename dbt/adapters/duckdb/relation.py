cd022563b419720cf314804772d0d9213f2e96c0
Josh Wills <josh@weavegrid.com>
2022-11-02 11:02:23 -0700

    checkpoint this for now

--- dbt/adapters/duckdb/relation.py
+++ dbt/adapters/duckdb/relation.py
@@ -3,7 +3,30 @@ from typing import Any, Optional, Type
 from dataclasses import dataclass
 
 from dbt.adapters.base.relation import BaseRelation, Self
+from dbt.exceptions import NotImplementedException
 from dbt.contracts.graph.parsed import ParsedSourceDefinition
+from dbt.contracts.graph.unparsed import ExternalTable
+
+
+def _create_external_location(external: ExternalTable) -> str:
+    location = external.location
+    if external.file_format:
+        if external.file_format == "parquet":
+            func = "read_parquet"
+        elif external.file_format == "csv":
+            func = "read_csv_auto"
+        else:
+            raise NotImplementedException(
+                f"Unsupported file format: {external.file_format}"
+            )
+        options = external.extra.get("options", None)
+        if options:
+            option_str = ", ".join([f"{k} = {v}" for k, v in options.items()])
+            return f"{func}('{location}', {option_str})"
+        else:
+            return f"{func}('{location}')"
+    else:
+        return f"'{location}'"
 
 
 @dataclass(frozen=True, eq=False, repr=False)
@@ -11,12 +34,12 @@ class DuckDBRelation(BaseRelation):
     external_location: Optional[str] = None
 
     @classmethod
-    def create_from_source(cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any) -> Self:
+    def create_from_source(
+        cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any
+    ) -> Self:
         if source.external:
-            # TODO(jwills): this is trivial and could be made much richer, just want to know
-            # if it works
-            kwargs["external_location"] = f"'{source.external.location}'"
-        return super().create_from_source(source, **kwargs)
+            kwargs["external_location"] = _create_external_location(source.external)
+        return super().create_from_source(source, **kwargs)  # type: ignore
 
     def render(self) -> str:
         if self.external_location:
