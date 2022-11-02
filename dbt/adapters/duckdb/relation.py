aab7acca7761b4a4c0f62be0ed036415be370a25
Josh Wills <josh@weavegrid.com>
2022-11-02 14:16:28 -0700

    Redo this as a jinja templated var

--- dbt/adapters/duckdb/relation.py
+++ dbt/adapters/duckdb/relation.py
@@ -3,30 +3,8 @@ from typing import Any, Optional, Type
 from dataclasses import dataclass
 
 from dbt.adapters.base.relation import BaseRelation, Self
-from dbt.exceptions import NotImplementedException
+from dbt.clients.jinja import get_environment
 from dbt.contracts.graph.parsed import ParsedSourceDefinition
-from dbt.contracts.graph.unparsed import ExternalTable
-
-
-def _create_external_location(external: ExternalTable) -> str:
-    location = external.location
-    if external.file_format:
-        if external.file_format == "parquet":
-            func = "read_parquet"
-        elif external.file_format == "csv":
-            func = "read_csv_auto"
-        else:
-            raise NotImplementedException(
-                f"Unsupported file format: {external.file_format}"
-            )
-        options = external.extra.get("options", None)
-        if options:
-            option_str = ", ".join([f"{k} = {v}" for k, v in options.items()])
-            return f"{func}('{location}', {option_str})"
-        else:
-            return f"{func}('{location}')"
-    else:
-        return f"'{location}'"
 
 
 @dataclass(frozen=True, eq=False, repr=False)
@@ -34,11 +12,14 @@ class DuckDBRelation(BaseRelation):
     external_location: Optional[str] = None
 
     @classmethod
-    def create_from_source(
-        cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any
-    ) -> Self:
-        if source.external:
-            kwargs["external_location"] = _create_external_location(source.external)
+    def create_from_source(cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any) -> Self:
+        if "external_location" in source.meta or "external_location" in source.source_meta:
+            env = get_environment()
+            if "external_location" in source.meta:
+                template = env.from_string(source.meta["external_location"])
+            else:
+                template = env.from_string(source.source_meta["external_location"])
+            kwargs["external_location"] = template.render(source.to_dict())
         return super().create_from_source(source, **kwargs)  # type: ignore
 
     def render(self) -> str:
