03051df36f173230608fd1af8d63484113169a02
Josh Wills <josh@weavegrid.com>
2022-11-02 22:57:06 -0700

    fix not in

--- dbt/adapters/duckdb/relation.py
+++ dbt/adapters/duckdb/relation.py
@@ -1,11 +1,7 @@
-
 from dataclasses import dataclass
-from typing import Any
-from typing import Optional
-from typing import Type
+from typing import Any, Optional, Type
 
-from dbt.adapters.base.relation import BaseRelation
-from dbt.adapters.base.relation import Self
+from dbt.adapters.base.relation import BaseRelation, Self
 from dbt.contracts.graph.parsed import ParsedSourceDefinition
 
 
@@ -14,7 +10,9 @@ class DuckDBRelation(BaseRelation):
     external_location: Optional[str] = None
 
     @classmethod
-    def create_from_source(cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any) -> Self:
+    def create_from_source(
+        cls: Type[Self], source: ParsedSourceDefinition, **kwargs: Any
+    ) -> Self:
 
         # Some special handling here to allow sources that are external files to be specified
         # via a `external_location` meta field. If the source's meta field is used, we include
@@ -31,7 +29,7 @@ class DuckDBRelation(BaseRelation):
             )
         if ext_location:
             # If it's a function call or already has single quotes, don't add them
-            if not "(" in ext_location and not ext_location.startswith("'"):
+            if "(" not in ext_location and not ext_location.startswith("'"):
                 ext_location = f"'{ext_location}'"
             kwargs["external_location"] = ext_location
 
