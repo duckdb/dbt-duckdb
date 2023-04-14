1ab241f598acd203b6dc3299a70f80ab54e6f5ff
Josh Wills <josh.wills@gmail.com>
2023-04-13 19:52:12 -0700

    Add in iceberg tests and some additional functionality for scan filtering

--- dbt/adapters/duckdb/plugins/iceberg.py
+++ dbt/adapters/duckdb/plugins/iceberg.py
@@ -8,10 +8,19 @@ from ..utils import SourceConfig
 
 class IcebergPlugin(Plugin):
     def __init__(self, config: Dict):
-        self._catalog = catalog.load_catalog(config.get("name"), config.get("properties"))
+        self._catalog = catalog.load_catalog(config.get("catalog"))
 
     def load(self, source_config: SourceConfig):
         table_format = source_config.meta.get("iceberg_table", "{schema}.{identifier}")
         table_name = table_format.format(**source_config.as_dict())
         table = self._catalog.load_table(table_name)
-        return table.scan().to_arrow()  # TODO: configurable scans
+        scan_keys = {
+            "row_filter",
+            "selected_fields",
+            "case_sensitive",
+            "snapshot_id",
+            "options",
+            "limit",
+        }
+        scan_config = {k: source_config.meta[k] for k in scan_keys if k in source_config.meta}
+        return table.scan(**scan_config).to_arrow()
--- tests/functional/plugins/test_iceberg.py
+++ tests/functional/plugins/test_iceberg.py
@@ -8,25 +8,17 @@ from dbt.tests.util import (
 sources_schema_yml = """
 version: 2
 sources:
-  - name: gsheet_source
+  - name: iceberg_source
     schema: main
     meta:
-      plugin: gsheet
-      title: "Josh's Test Spreadsheet"
+      plugin: icee
+      iceberg_table: "examples.{identifier}"
     tables:
-      - name: gsheet1
-        description: "My first sheet"
-      - name: gsheet2
-        description: "The second sheet in the doc"
-        meta:
-          worksheet: "TwoSheet"
+      - name: nyc_taxi_locations
 """
 
 models_source_model1_sql = """
-    select * from {{ source('gsheet_source', 'gsheet1') }}
-"""
-models_source_model2_sql = """
-    select * from {{ source('gsheet_source', 'gsheet2') }}
+    select * from {{ source('iceberg_source', 'nyc_taxi_locations') }}
 """
 
 
@@ -35,10 +27,10 @@ models_source_model2_sql = """
 # wrong with profiles_config_update since it can't be used in multiple
 # tests in the same pytest session
 @pytest.mark.skip
-class TestGSheetPlugin:
+class TestIcebergPlugin:
     @pytest.fixture(scope="class")
     def profiles_config_update(self, dbt_profile_target):
-        config = {"method": "oauth"}
+        config = {"catalog": "default"}
         if "path" not in dbt_profile_target:
             return {}
         return {
@@ -48,7 +40,7 @@ class TestGSheetPlugin:
                         "type": "duckdb",
                         "path": dbt_profile_target["path"],
                         "plugins": [
-                            {"name": "gsheet", "impl": "gsheet", "config": config}
+                            {"name": "icee", "impl": "iceberg", "config": config}
                         ],
                     }
                 },
@@ -57,29 +49,23 @@ class TestGSheetPlugin:
         }
 
     @pytest.fixture(scope="class")
-    def models(self, test_data_path):
+    def models(self):
         return {
-            "schema.yml": sources_schema_yml.format(test_data_path=test_data_path),
+            "schema.yml": sources_schema_yml,
             "source_model1.sql": models_source_model1_sql,
-            "source_model2.sql": models_source_model2_sql,
         }
 
-    def test_gshseet_plugin(self, project):
+    def test_iceberg_plugin(self, project):
         results = run_dbt()
-        assert len(results) == 2
+        assert len(results) == 1
+
+        res = project.run_sql("SELECT COUNT(1) FROM nyc_taxi_locations", fetch="one")
+        assert res[0] == 265
 
         check_relations_equal(
             project.adapter,
             [
-                "gsheet1",
+                "nyc_taxi_locations",
                 "source_model1",
-            ],
-        )
-
-        check_relations_equal(
-            project.adapter,
-            [
-                "gsheet2",
-                "source_model2",
             ],
         )
