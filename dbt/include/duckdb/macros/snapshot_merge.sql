bfe1226a279bfc53d57076d72c40277c86790840
Josh Wills <josh.wills@gmail.com>
2023-06-11 08:53:59 -0700

    Add an alias here for the update target to get around whatever is broken when running against MD

--- dbt/include/duckdb/macros/snapshot_merge.sql
+++ dbt/include/duckdb/macros/snapshot_merge.sql
@@ -1,12 +1,12 @@
 {% macro duckdb__snapshot_merge_sql(target, source, insert_cols) -%}
     {%- set insert_cols_csv = insert_cols | join(', ') -%}
 
-    update {{ target }}
+    update {{ target }} as TARGET
     set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
     from {{ source }} as DBT_INTERNAL_SOURCE
-    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = {{ target }}.dbt_scd_id::text
+    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = TARGET.dbt_scd_id::text
       and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
-      and {{ target }}.dbt_valid_to is null;
+      and TARGET.dbt_valid_to is null;
 
     insert into {{ target }} ({{ insert_cols_csv }})
     select {% for column in insert_cols -%}
