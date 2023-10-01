14558bc3c8f64d1c5c7e3ce39094ff42c23b2ba7
Aleksandar Milicevic <milicoa97@gmail.com>
2023-10-01 21:24:17 +0000

    add time travel; remote storage (should test)

--- dbt/adapters/duckdb/plugins/delta.py
+++ dbt/adapters/duckdb/plugins/delta.py
@@ -10,20 +10,35 @@ from ..utils import SourceConfig
 
 class Plugin(BasePlugin):
     def initialize(self, config: Dict[str, Any]):
+        #place for init catalog in the future
         pass
 
     def load(self, source_config: SourceConfig):
         if "delta_table_path" not in source_config:
             raise Exception("'delta_table_path' is a required argument for the delta table!")
-        
+        print(source_config)
         table_path = source_config["delta_table_path"]
+        storage_options = source_config.get("storage",None)
+        
+        if storage_options: 
+            dt = DeltaTable(table_path, storage_options)
+        else:
+            dt = DeltaTable(table_path)
 
+        #delta attributes
+        as_of_version = source_config.get("as_of_version", None)
+        as_of_datetime = source_config.get("as_of_datetime", None)
+
+        if as_of_version:
+            dt.load_version(1)
+        
+        if as_of_datetime:
+            dt.load_with_datetime(as_of_datetime)
+
+        #prunning attributes
         pruning_filter = source_config.get("pruning_filter", "1=1")
         pruning_projection = source_config.get("pruning_projection", "*")
 
-        ##TODO check if table is there and path is ok
-
-        dt = DeltaTable(table_path)
         df_db = duckdb.arrow(dt.to_pyarrow_table())
         df_db_pruned = df_db.filter(pruning_filter).project(pruning_projection)
 
@@ -33,5 +48,5 @@ class Plugin(BasePlugin):
 #TODO each node calls plugin.load indipendent which is maybe overhead?
 
 #Future
-#TODO add time travel; add deltalake storage options 
+#TODO add deltalake storage options 
 #TODO add databricks catalog 
