a79c482e619344e2c6982c95c815ce8439d96e03
Josh Wills <josh.wills@gmail.com>
2023-10-24 12:20:13 -0700

    Add in all of the seed delimiter tests

--- tests/functional/adapter/simple_seed/test_fast_seed.py
+++ tests/functional/adapter/simple_seed/test_fast_seed.py
@@ -23,3 +23,29 @@ class TestSeedWithUniqueDelimiter(SeedUniqueDelimiterTestBase):
         """Testing correct run of seeds with a unique delimiter (pipe in this case)"""
         self._build_relations_for_test(project)
         self._check_relation_end_state(run_result=run_dbt(["seed"]), project=project, exists=True)
+
+
+class TestSeedWithWrongDelimiter(SeedUniqueDelimiterTestBase):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "seeds": {"quote_columns": False, "delimiter": ";"},
+        }
+
+    def test_seed_with_wrong_delimiter(self, project):
+        """Testing failure of running dbt seed with a wrongly configured delimiter"""
+        seed_result = run_dbt(["seed"], expect_pass=False)
+        assert "syntax error" in seed_result.results[0].message.lower()
+
+
+class TestSeedWithEmptyDelimiter(SeedUniqueDelimiterTestBase):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "seeds": {"quote_columns": False, "delimiter": ""},
+        }
+
+    def test_seed_with_empty_delimiter(self, project):
+        """Testing failure of running dbt seed with an empty configured delimiter value"""
+        seed_result = run_dbt(["seed"], expect_pass=False)
+        assert "compilation error" in seed_result.results[0].message.lower()
