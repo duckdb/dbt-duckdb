971ef70cf82463237f61cf6decfa80fd8eb07895
Josh Wills <josh.wills@gmail.com>
2023-10-24 12:17:36 -0700

    Support delimiter options inside of the dbt-duckdb fast seed loader

--- tests/functional/adapter/simple_seed/test_fast_seed.py
+++ tests/functional/adapter/simple_seed/test_fast_seed.py
@@ -1,6 +1,7 @@
 import pytest
 
 from dbt.tests.adapter.simple_seed.test_seed import SeedTestBase
+from dbt.tests.adapter.simple_seed.test_seed import SeedUniqueDelimiterTestBase
 from dbt.tests.util import (
     run_dbt,
 )
@@ -13,5 +14,12 @@ class TestSeedConfigFast(SeedTestBase):
         }
 
     def test_simple_seed_fast(self, project):
+        self._build_relations_for_test(project)
+        self._check_relation_end_state(run_result=run_dbt(["seed"]), project=project, exists=True)
+
+
+class TestSeedWithUniqueDelimiter(SeedUniqueDelimiterTestBase):
+    def test_seed_with_unique_delimiter(self, project):
+        """Testing correct run of seeds with a unique delimiter (pipe in this case)"""
         self._build_relations_for_test(project)
         self._check_relation_end_state(run_result=run_dbt(["seed"]), project=project, exists=True)
