5f594c40292c3dd7a7266b9931e8a5cad9a5b736
Jeremy Cohen <jeremy@dbtlabs.com>
2024-04-16 11:27:01 +0200

    Readd dbt-core to setup.py for install back-compat

--- setup.py
+++ setup.py
@@ -37,9 +37,11 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        "dbt-common>=1.0.0b1,<2.0",
-        "dbt-adapters>=1.0.0b1,<2.0",
+        "dbt-common>=1,<2",
+        "dbt-adapters>=1,<2",
         "duckdb>=0.7.0",
+        # add dbt-core to ensure backwards compatibility of installation, this is not a functional dependency
+        "dbt-core>=1.8.0b1",
     ],
     extras_require={"glue": ["boto3", "mypy-boto3-glue"], "md": ["duckdb>=0.7.0,<=0.9.2"]},
     classifiers=[
