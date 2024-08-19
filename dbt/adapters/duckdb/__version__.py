81fc615cf3780e7a0a6db0bc804de4ad5302d1a1
Guen Prawiroatmodjo <guen@motherduck.com>
2024-08-19 11:24:17 -0700

    also replace a/b/c

--- dbt/adapters/duckdb/__version__.py
+++ dbt/adapters/duckdb/__version__.py
@@ -4,4 +4,6 @@ _package_name = "dbt-duckdb"
 version = get_version(_package_name)
 # This is to get around SemVer 2 (dbt_common) vs Linux/Python compatible SemVer 3 (pbr) conflicting
 # See: https://docs.openstack.org/pbr/latest/user/semver.html
-version = version.replace(".dev", "-dev")
+_prerelease_tags = ["dev", "a", "b", "c"]
+for tag in _prerelease_tags:
+    version = version.replace(f".{tag}", f"-{tag}")
