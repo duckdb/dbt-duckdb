40c4ec71d2e83cd796bdbe3e6cd1ede8b3bf041d
Guen Prawiroatmodjo <guen@motherduck.com>
2024-08-19 11:18:38 -0700

    make package_name a private var instead of dunder

--- dbt/adapters/duckdb/__version__.py
+++ dbt/adapters/duckdb/__version__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version as get_version
 
-__package_name__ = "dbt-duckdb"
-version = get_version(__package_name__)
+_package_name = "dbt-duckdb"
+version = get_version(_package_name)
 # This is to get around SemVer 2 (dbt_common) vs Linux/Python compatible SemVer 3 (pbr) conflicting
 # See: https://docs.openstack.org/pbr/latest/user/semver.html
 version = version.replace(".dev", "-dev")
