779b2dc40cab5475be18fe05a8c637a416f64f9f
Guen Prawiroatmodjo <guen@motherduck.com>
2024-08-19 11:16:21 -0700

    drop pbr runtime requirement

--- dbt/adapters/duckdb/__version__.py
+++ dbt/adapters/duckdb/__version__.py
@@ -1,6 +1,7 @@
-from pbr.version import VersionInfo
+from importlib.metadata import version as get_version
 
-package_name = "dbt-duckdb"
-info = VersionInfo(package_name)
-
-version = info.version_string()
+__package_name__ = "dbt-duckdb"
+version = get_version(__package_name__)
+# This is to get around SemVer 2 (dbt_common) vs Linux/Python compatible SemVer 3 (pbr) conflicting
+# See: https://docs.openstack.org/pbr/latest/user/semver.html
+version = version.replace(".dev", "-dev")
