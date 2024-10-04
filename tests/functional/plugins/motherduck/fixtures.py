bdb67703060aac5ad08e1a929c4f1956a5795253
Guen Prawiroatmodjo <guen@motherduck.com>
2024-10-04 13:56:24 -0700

    add macro test

---
+++ tests/functional/plugins/motherduck/fixtures.py
@@ -0,0 +1,27 @@
+#
+# Models
+#
+
+models__gen_data_macro = """
+select * from {{ ref("seed") }}
+"""
+
+#
+# Macros
+#
+
+macros__generate_database_name = """
+{% macro generate_database_name(custom_database_name=none, node=none) -%}
+    {{ target.database | trim }}_{{ var("build_env") | trim }}_{{ var("org_prefix") | trim }}
+{%- endmacro %}
+"""
+
+#
+# Seeds
+#
+
+seeds__example_seed_csv = """a,b,c
+1,2,3
+4,5,6
+7,8,9
+"""
---
+++ tests/functional/plugins/motherduck/test_macros.py
@@ -0,0 +1,64 @@
+from urllib.parse import urlparse
+import pytest
+
+from dbt.tests.util import (
+    run_dbt,
+    check_result_nodes_by_name
+)
+from tests.functional.plugins.motherduck.fixtures import (
+    models__gen_data_macro,
+    macros__generate_database_name,
+    seeds__example_seed_csv,
+)
+
+
+class TestMacrosGenerateDatabaseName:
+    @pytest.fixture(scope="class")
+    def database_name(self, dbt_profile_target):
+        return urlparse(dbt_profile_target["path"]).path + "_ducky_ducky"
+
+    @pytest.fixture(autouse=True)
+    def run_dbt_scope(self, project, database_name):
+        project.run_sql(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+        yield
+        project.run_sql(f"DROP DATABASE {database_name}")
+
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "seed.csv": seeds__example_seed_csv,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "model.sql": models__gen_data_macro
+        }
+
+    @pytest.fixture(scope="class")
+    def macros(self):
+        return {"db_name.sql": macros__generate_database_name}
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "config-version": 2,
+            "vars": {
+                "test": {
+                    "build_env": "DUCKY",
+                    "org_prefix": "DUCKY"
+                },
+            },
+            "macro-paths": ["macros"],
+        }
+
+    def test_dbname_macro(self, project):
+        # seed command
+        results = run_dbt(["seed"])
+        assert len(results) == 1
+        check_result_nodes_by_name(results, ["seed"])
+
+        # run command
+        results = run_dbt(["run"])
+        assert len(results) == 1
+        check_result_nodes_by_name(results, ["model"])
