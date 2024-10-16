2a067efe28a00095704ce17b3e7e59e96db67614
Arno Roos <Arno.Roos@vrt.be>
2024-10-16 09:13:42 +0000

    auto_detect=true default values for json and csv

--- dbt/include/duckdb/macros/materializations/external.sql
+++ dbt/include/duckdb/macros/materializations/external.sql
@@ -6,8 +6,8 @@
   {%- set write_options = adapter.external_write_options(location, rendered_options) -%}
   {%- set read_location = adapter.external_read_location(location, rendered_options) -%}
   {%- set parquet_read_options = config.get('parquet_read_options', {'union_by_name': False}) -%}
-  {%- set json_read_options = config.get('json_read_options', {'auto_detect': False}) -%}
-  {%- set csv_read_options = config.get('csv_read_options', {'auto_detect': False}) -%}
+  {%- set json_read_options = config.get('json_read_options', {'auto_detect': True}) -%}
+  {%- set csv_read_options = config.get('csv_read_options', {'auto_detect': True}) -%}
 
   -- set language - python or sql
   {%- set language = model['language'] -%}
