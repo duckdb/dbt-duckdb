039b341eb1499ca973489584e602a9470de4eaff
Alex-Monahan <alex@motherduck.com>
2025-03-27 16:25:31 -0700

    Test alter table add column works without recreate

--- tests/functional/adapter/test_table_function.py
+++ tests/functional/adapter/test_table_function.py
@@ -6,7 +6,8 @@ import pytest
 
 from dbt.tests.util import (
     run_dbt,
-    check_result_nodes_by_name
+    check_result_nodes_by_name,
+    relation_from_name
 )
 
 seeds__example_seed_csv = """a,b,c
@@ -67,15 +68,23 @@ models__use_table_function_2_params = """
 select * from {{ ref("my_table_function_2_params") }}(4, 5)
 """
 
-# To truely test the behavior of adding columns working, I need to:
-#   Create a table_function that is a select * from an example table
-#   Alter that table to add a column
-#   Query the table_function without error (force order of operations with  -- depends_on: {{ ref('upstream_parent_model') }} )
-# models__use_table_function_after_adding_column = """
-# {{ config(materialized='table') }}
-# {% set results = run_query('alter table {{ ref("example_table") }} add column c default 42) %}
-# select * from {{ ref("my_table_function") }}()
-# """
+# To test that the table function will work smoothly even if a column is added:
+#   Create an example_table
+#   create a table_function that is select * from example_table
+#   Persist the output of that table function to a table
+#   Alter the table to add a column
+#   Persist the output of that table function to a new table (should include the new column)
+# Note this will not recreate the table_function (which would have been needed with a view)
+models__use_table_function_after_adding_column = """
+-- depends_on: {{ ref('use_table_function') }}
+{{ config(materialized='table') }}
+{% set alter_table_query %}
+alter table {{ ref("example_table") }} add column d integer default 42
+{% endset %}
+
+{% set results = run_query(alter_table_query) %}
+select * from {{ ref("my_table_function") }}()
+"""
 
 
 
@@ -100,6 +109,7 @@ class TestTableFunction:
             "use_table_function_1_param_with_comma.sql": models__use_table_function_1_param_with_comma,
             "my_table_function_2_params.sql": models__my_table_function_2_params,
             "use_table_function_2_params.sql": models__use_table_function_2_params,
+            "use_table_function_after_adding_column.sql": models__use_table_function_after_adding_column,
         }
 
     def test_base(self, project):
@@ -109,7 +119,7 @@ class TestTableFunction:
         check_result_nodes_by_name(results, ["seed"])
 
         results = run_dbt(["run"])
-        assert len(results) == 9
+        assert len(results) == 10
         check_result_nodes_by_name(results, [
             "example_table",
             "my_table_function",
@@ -120,4 +130,15 @@ class TestTableFunction:
             "use_table_function_1_param_with_comma",
             "my_table_function_2_params",
             "use_table_function_2_params",
+            "use_table_function_after_adding_column"
         ])
+
+        relation_pre_alter = relation_from_name(project.adapter, "use_table_function")
+        result_pre_alter = project.run_sql(f"describe {relation_pre_alter}", fetch="all")
+        column_names_pre_alter = [row[0] for row in result_pre_alter]
+        assert column_names_pre_alter == ['a', 'b', 'c']
+
+        relation_post_alter = relation_from_name(project.adapter, "use_table_function_after_adding_column")
+        result_post_alter = project.run_sql(f"describe {relation_post_alter}", fetch="all")
+        column_names_post_alter = [row[0] for row in result_post_alter]
+        assert column_names_post_alter == ['a', 'b', 'c', 'd']
