340e9ae3745a114d623ab9b1a11c135c246cb5e3
Guen Prawiroatmodjo <guen@motherduck.com>
2025-04-20 11:01:18 -0700

    remove noop

--- dbt/include/duckdb/macros/materializations/hooks.sql
+++ dbt/include/duckdb/macros/materializations/hooks.sql
@@ -1,9 +1,5 @@
 {% macro run_hooks(hooks, inside_transaction=True) %}
   {% for hook in hooks | selectattr('transaction', 'equalto', inside_transaction)  %}
-    {% if not inside_transaction and loop.first %}
-      {% call statement(auto_begin=inside_transaction) %}
-      {% endcall %}
-    {% endif %}
     {% set rendered = render(hook.get('sql')) | trim %}
     {% if (rendered | length) > 0 %}
       {% call statement(auto_begin=inside_transaction) %}
