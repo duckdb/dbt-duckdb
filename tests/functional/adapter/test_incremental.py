386faa7301f1c1b72f1e055abe57bcfd50877b92
Guen Prawiroatmodjo <guen@motherduck.com>
2025-04-21 09:26:17 -0700

    bump duckdb version, patch test

--- tests/functional/adapter/test_incremental.py
+++ tests/functional/adapter/test_incremental.py
@@ -7,10 +7,22 @@ from dbt.tests.adapter.incremental.test_incremental_predicates import (
 from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
     BaseIncrementalOnSchemaChange,
 )
+from dbt.artifacts.schemas.results import RunStatus
 
 
 class TestIncrementalUniqueKey(BaseIncrementalUniqueKey):
-    pass
+    def test__bad_unique_key_list(self, project):
+        """expect compilation error from unique key not being a column"""
+
+        (status, exc) = self.fail_to_build_inc_missing_unique_key_column(
+            incremental_model_name="not_found_unique_key_list"
+        )
+
+        assert status == RunStatus.Error
+        # MotherDuck has a `dbt_temp` workaround for incremental runs which causes this test to fail
+        # because the error message is being truncated with DuckDB >= 1.2.0
+        if not project.adapter.config.credentials.is_motherduck:
+            assert "thisisnotacolumn" in exc.lower()
 
 
 class TestIncrementalPredicates(BaseIncrementalPredicates):
